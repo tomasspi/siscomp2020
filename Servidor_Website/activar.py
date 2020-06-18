8d67a2c4267f70ef91c6bd30c5070e0d402b5f88
Tomas <tom-300@hotmail.com>
2020-06-17 21:05:43 -0300

    Refactor

--- Servidor_Website/activar.py
+++
@@ -1,7 +0,0 @@
-import time
-import os
-
-output= "Se activo el proceso..."
-print(output)
-
-os.system("kill -CONT 23176") # Activa proceso
--- Servidor_Website/desactivar.py
+++
@@ -1,7 +0,0 @@
-import time
-import os
-
-output= "Se desactivo el proceso..."
-print(output)
-
-os.system("kill -STOP 23176") # Desactiva proceso
