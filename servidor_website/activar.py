7f76c4dfaf0d26b45c3dfc194a77617565de0610
Tomas <tom-300@hotmail.com>
2020-06-17 21:04:20 -0300

    Refactor en nombre y mejoras en el servidor

---
+++ servidor_website/activar.py
@@ -0,0 +1,7 @@
+import time
+import os
+
+output= "Se activo el proceso..."
+print(output)
+
+os.system("kill -CONT 23176") # Activa proceso
---
+++ servidor_website/desactivar.py
@@ -0,0 +1,7 @@
+import time
+import os
+
+output= "Se desactivo el proceso..."
+print(output)
+
+os.system("kill -STOP 23176") # Desactiva proceso
